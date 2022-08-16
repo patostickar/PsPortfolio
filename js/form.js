7c3739b3a4983dd5e9443abb17598cc6f8c9aa3a
Patricio Stickar <58763654+patostickar@users.noreply.github.com>
2022-08-15 21:54:28 -0300

    form connected to SMTP and validation

---
+++ js/form.js
@@ -0,0 +1,55 @@
+(function () {
+  emailjs.init('lpOntDQ5WI-TQQtIM');
+})();
+(function () {
+  'use strict';
+  window.addEventListener(
+    'load',
+    function () {
+      // Get the forms we want to add validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener(
+          'submit',
+          function (event) {
+            event.preventDefault();
+
+            let alert = document.getElementById('email-alert');
+
+            if (form.checkValidity() === false) {
+              event.stopPropagation();
+              form.classList.add('was-validated');
+            } else {
+              this.contact_number.value = (Math.random() * 100000) | 0;
+
+              emailjs.sendForm('service_9rz4q2w', 'template_g1qb1zn', this).then(
+                function () {
+                  form.classList.remove('was-validated');
+
+                  alert.textContent = 'Message sent';
+                  alert.classList.add('alert-success');
+                  $('.alert').addClass('show');
+                  setTimeout(function () {
+                    $('.alert').removeClass('show');
+                  }, 5000);
+                },
+
+                function (error) {
+                  alert.textContent = error;
+                  alert.classList.add('alert-danger');
+                  $('.alert').addClass('show');
+                  setTimeout(function () {
+                    $('.alert').removeClass('show');
+                  }, 5000);
+                }
+              );
+            }
+          },
+          false
+        );
+      });
+    },
+    false
+  );
+})();
